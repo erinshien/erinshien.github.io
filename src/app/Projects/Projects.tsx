2002e26b8dee2577b29a9b8bdce681c1b909c7b7
Erin Shien <erinshiensmith@gmail.com>
2024-04-22 21:01:59 +0100

    created projects component

---
+++ src/app/Projects/Projects.tsx
@@ -0,0 +1,16 @@
+import 'react';
+import './Projects.module.css';
+
+import ProjectCard from '../ProjectCard/ProjectCard';
+
+export default function Projects() {
+  return (
+    <>
+      <h2>MY PROJECTS</h2>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </>
+  )
+}
