bcb253c5064b675d231b0a0cc09469699c50a2f0
Erin Shien <erinshiensmith@gmail.com>
2024-04-22 21:01:49 +0100

    created title component

---
+++ src/app/Title/Title.tsx
@@ -0,0 +1,11 @@
+import 'react';
+import './Title.module.css';
+
+export default function Title() {
+  return (
+    <>
+      <h1>ERIN SHIEN SMITH</h1>
+      <p>Full Stack Developer & Designer</p>
+    </>
+  )
+}
