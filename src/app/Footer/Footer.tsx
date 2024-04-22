054b4e0d2bc1b63fa0cca99dbf59bb0d3c7b35cf
Erin Shien <erinshiensmith@gmail.com>
2024-04-22 21:22:28 +0100

    created footer component

---
+++ src/app/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import 'react';
+import './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <>
+      <p>Devloped with love and React ✨ Erin Shien Smith, 2024</p>
+    </>
+  )
+}
