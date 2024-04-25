963457dc1c4dcfcc993d8a955b3b9017e9fc882d
Erin Shien <erinshiensmith@gmail.com>
2024-04-25 21:11:12 +0100

    amended Title and Nav styling

--- src/app/Title/Title.tsx
+++ src/app/Title/Title.tsx
@@ -5,14 +5,13 @@ import Image from 'next/image'
 export default function Title() {
   return (
     <section className={styles.title}>
-      <Image className={styles.image}
+      <h1 className={styles.firstNames}>ERIN SHIEN<Image className={styles.image}
         priority
         src="/image1.png"
         width={350}
         height={300}
         alt="Picture of the author"
-      />
-      <h1 className={styles.firstNames}>ERIN SHIEN</h1>
+      /></h1>
       <h1 className={styles.surname}>SMITH</h1>
       <p className={styles.p}>Full Stack Developer & Designer</p>
     </section>
